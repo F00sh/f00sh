be24f303c7cd5cef6fac9e0e6ade9d43faab7245
F00sh <fooshmoola@gmail.com>
2026-06-01 19:52:38 +0200

    fit

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,17 @@
-﻿<template>
-  <main class="relative min-h-screen bg-neutral-950 text-neutral-50">
+<template>
+  <main
+    class="relative bg-neutral-950 text-neutral-50"
+    :class="isHome ? 'min-h-screen' : 'h-screen h-[100svh] h-[100dvh] overflow-hidden flex flex-col'"
+  >
     <SiteHeader />
-    <NuxtPage />
+    <div :class="isHome ? '' : 'flex-1 min-h-0 overflow-hidden'">
+      <NuxtPage />
+    </div>
     <SiteFooter />
   </main>
 </template>
 
+<script setup lang="ts">
+const route = useRoute();
+const isHome = computed(() => route.path === '/');
+</script>
